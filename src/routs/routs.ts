5cf243ce85735954155c68a0c6b7eb8bceb7c28d
mohammad <m.ashtiani.06s@gmail.com>
2023-09-25 18:08:19 +0330

    create identity layout

--- src/routs/routs.ts
+++ src/routs/routs.ts
@@ -1,17 +1,25 @@
 import { createBrowserRouter } from "react-router-dom";
 import Login from "../features/identity/login/login";
 import Register from "../features/identity/register/register";
+import IdentityLayout from "../layout/identity-layout/identity-layout";
 
 const router = createBrowserRouter([
-  {
-    path: 'login',
-    Component: Login
-  },
-  {
-    path: 'register',
-    Component: Register
-  },
-  
+    {
+        Component: IdentityLayout,
+        children: [
+            {
+                path: 'login',
+                Component: Login
+            },
+            {
+                path: 'register',
+                Component: Register
+            },
+
+        ]
+    }
+
+
 ]);
 
 export default router;
